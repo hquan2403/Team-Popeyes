3216037d20ba929481a75d5e233bd4d1858fcd38
hquan2403 <5001104127@student.hcmue.edu.vn>
2025-09-26 21:16:24 +0700

    Update Testpush.cpp

--- Testpush.cpp
+++ Testpush.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int main() {
     cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
     int a,b;
     return 0;
 } 
+
