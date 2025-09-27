1da1c9e9ec8460505521f45ee123ed7aff372cdf
hquan2403 <hquan2403@gmail.com>
2025-09-27 09:14:18 +0700

    len xe di nhoc

--- Testpush.cpp
+++ Testpush.cpp
@@ -8,7 +8,16 @@ int main() {
     cout<<"Vay la lan cuoi di ben nhau"<<endl;
     cout<<"Vay la lan cuoi di ben nhau"<<endl;
     cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    cout<<"Vay la lan cuoi di ben nhau"<<endl;
+    int c=6;
+    cout<<c;
     int a,b;
     return 0;
+    
 } 
-
+    
