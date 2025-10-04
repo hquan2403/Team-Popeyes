145783927c4909172262a0637f7126e646fa34ec
nguyenthanhphong30052k6-cmyk <nguyethanhphong30052k6@gmail.com>
2025-10-04 07:16:54 +0700

    test

--- Project/Project_2.cpp
+++ Project/Project_2.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
-    cout << "Hello, World!" << endl;
-    cout<<"MU VO DICH EPL";
-    return 0;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6;
+
+// Đếm nguyên tố từ 0 đến 25
+bool nt(int n) {
+	if(n < 2) {
+		return false;
+	}
+	for(int i = 2; i <= sqrt(n); i++) {
+		if(n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int n;
+	cin>>n;
+	for(int i = 1; i <= n; i++) {
+		if(nt(i)) {
+			cout<<i<<" ";
+		}
+	}
+
 }
