9911461555fa6735959473116f874bdfadcc0adc
Ndtinh206 <5001104163@student.hcmue.edu.vn>
2025-10-06 12:52:51 +0700

    admin depzai so 1

--- Testpush.cpp
+++ Testpush.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+bool snt(int n){
+    if(n<2) return false;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
 int main() {
-    cout<<"Hello World";
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(snt(a[i])) cout<<a[i]<<" ";
+        else cout<<-1<<" ";
+    }
     return 0;
 } 
-    
 
